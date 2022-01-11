eae7e9f1a85764c5690771d314064c84163301d7
mario <mpd651@gmail.com>
2022-01-11 17:01:43 +0100

    11 enero segunda actualizacion clase

--- TEMA4/hoja5/hoja5Ejercicio2/src/main/java/es/marioperez/hoja5ejercicio2/Main.java
+++ TEMA4/hoja5/hoja5Ejercicio2/src/main/java/es/marioperez/hoja5ejercicio2/Main.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 /**
  *
@@ -12,14 +13,31 @@ import java.sql.Statement;
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException
     {
+        Scanner tecladoi=new Scanner(System.in);
+        Scanner teclados=new Scanner(System.in);
         Connection conexion = Conexion.getInstance().getConnection();
         
         String sql="SELECT usuario,fecha,cancion FROM votos ORDER BY fecha DESC LIMIT 5;";
-        Statement consulta=conexion.createStatement();
+        Statement consulta=conexion.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
         ResultSet result=consulta.executeQuery(sql);
         
         while (result.next()){
-            System.out.println("");
+            System.out.println("Usuario: "+result.getString("usuario")+", fecha: "+result.getDate("fecha").toLocalDate()+", cancion: "+result.getInt("cancion"));
+            System.out.println("Introduce una opcion");
+            System.out.println("1.Modificar usuario\n"
+                    + "2.Eliminar");
+            int opcion=tecladoi.nextInt();
+            
+            switch (opcion){
+                case 1:
+                    System.out.println("Introduce el nuevo usuario: ");
+                    result.updateString("usuario", teclados.nextLine());
+                    result.updateRow();
+                    break;
+                case 2:
+                    result.deleteRow();
+                    break;
+            }
         }
     }
 }
