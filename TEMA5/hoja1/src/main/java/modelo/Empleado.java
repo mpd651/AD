2fe2557f9f0d0e3a2b2ac62ce08168171d61b4e8
mario <mpd651@gmail.com>
2022-01-28 17:31:30 +0100

    clase

---
+++ TEMA5/hoja1/src/main/java/modelo/Empleado.java
@@ -0,0 +1,57 @@
+package modelo;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="empelados")
+public class Empleado {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(length = 50, nullable = true)
+	private String nombre;
+	private String oficio;
+	private LocalDate fechaAlta;
+	private double salario;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getOficio() {
+		return oficio;
+	}
+	public void setOficio(String oficio) {
+		this.oficio = oficio;
+	}
+	public LocalDate getFechaAlta() {
+		return fechaAlta;
+	}
+	public void setFechaAlta(LocalDate fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
+}
